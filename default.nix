896eec47b60413172be23e3ccdcc32e4edec4487
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-06-15 10:11:23 +0900

    chore: update vendorHash

--- default.nix
+++ default.nix
@@ -5,5 +5,5 @@ pkgs.buildGoLatestModule {
   pname = "gh-secrets-manager";
   version = "0.0.4";
   src = ./.;
-  vendorHash = "sha256-qKthz37Tu8SrDV0iA+nHWVRzeOmwNcZjKbBbRenP/4Q=";
+  vendorHash = "sha256-uo+8aHzvmiIGteF1vwF69D0T8U7/zaOThcdusjmXfto=";
 }
